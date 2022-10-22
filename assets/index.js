29c2a7a1200bc1d3279208928a40e1e5528cdf4e
Peter Egbujie <ogbillion@gmail.com>
2022-10-22 15:26:11 -0700

    Created desktop and mobile navigation

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+import arrow from "./arrow_right.svg";
+import backup from "./Backup-logo-nav.svg";
+import docSend from "./DocSend-logo-nav.svg";
+import dropbox from "./Dropbox-logo-nav.svg";
+import logo from "./Dropbox_logo.svg.png";
+import helloSign from "./HelloSign-logo-nav.svg";
+
+export { logo, arrow, backup, docSend, helloSign, dropbox };
---
+++ constants/index.tsx
@@ -0,0 +1,135 @@
+import { arrow, backup, docSend, dropbox, helloSign } from "../assets";
+
+export const navLinks1 = [
+ {
+  id: "home",
+  title: "Why Dropbox?",
+ },
+ {
+  id: "products",
+  title: "Products",
+ },
+ {
+  id: "solutions",
+  title: "Solutions",
+ },
+ {
+  id: "pricing",
+  title: "Pricing",
+ },
+];
+
+export const navLinks2 = [
+ {
+  id: "Sign up",
+  title: "Sign up",
+ },
+ {
+  id: "sign in",
+  title: "Sign in",
+ },
+];
+
+export const Why = [
+ {
+  id: "overview",
+  title: "Overview",
+ },
+ {
+  id: "get-inspired",
+  title: "Get Inspired",
+ },
+ {
+  id: "customers",
+  title: "What customer say",
+ },
+ {
+  id: "app-integration",
+  title: "App integration",
+ },
+ {
+  id: "resources",
+  title: "Resources",
+ },
+ {
+  id: "better",
+  title: "Better with Dropbox",
+ },
+ {
+  id: "share",
+  title: "Share files",
+ },
+ {
+  id: "store",
+  title: "Store and organize",
+ },
+ {
+  id: "sync",
+  title: "Sync your devices",
+ },
+ {
+  id: "protect",
+  title: "Protect and secure data",
+ },
+];
+
+export const Products = [
+ {
+  img: dropbox,
+  title: "Dropbox",
+  subtitle: "Store,share, and access files across devices",
+ },
+ {
+  img: backup,
+  title: "Backup",
+  subtitle: "Store,share, and access files across devices",
+ },
+ {
+  img: helloSign,
+  title: "HelloSign",
+  subtitle: "Request and add signatures to documents",
+ },
+ {
+  img: docSend,
+  title: "DocSend",
+  subtitle: "Send documents securely nd track activity",
+ },
+ {
+  img: arrow,
+  title: "Advance access",
+  subtitle: "Preview new product experiences",
+ },
+];
+
+export const Solutions = [
+ "Creatives",
+ "Sales",
+ "Marketing",
+ " HR",
+ "IT",
+ "Team Lead",
+ "Personal",
+];
+
+export const Contact = [
+ {
+  id: "support",
+  title: "Support",
+ },
+ {
+  id: "help",
+  title: "Help center",
+ },
+ {
+  id: "sales",
+  title: "Contact Sales",
+ },
+ {
+  id: "email",
+  title: "Email",
+ },
+ {
+  id: "phone",
+  title: 8006205395,
+ },
+];
